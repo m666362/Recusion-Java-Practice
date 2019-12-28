9632ecbd9801e985abf9e579c43c59e3a159748f
Md Rayhan <mdrayhan9464@gmail.com>
2019-12-28 15:26:14 +0600

    initial code done

--- recursion_lib/src/main/java/org/richit/recursion_lib/MyClass.java
+++ recursion_lib/src/main/java/org/richit/recursion_lib/MyClass.java
@@ -12,7 +12,15 @@ public class MyClass {
     public static void main(String[] args) {
         headingProgram("Start Programme");
 
+        System.out.println("The factorial is : " + calculateFactorial( 0 ));
 
         headingProgram( "End Programme" );
+    }
+
+
+    private static int calculateFactorial(int value) {
+        if(value==1 || value ==0)
+            return 1;
+        return calculateFactorial( value-1 )*value;
     }
 }
